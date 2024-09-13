2a4e9fb56ab377687d1fe5e928d3ada413177c2f
Jennifer Loza-Marin <jenniferlozamarin@usf.edu>
2024-09-13 13:17:52 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,12 @@
+// Task 1: Create an Inventory Array of Product Objects
+
+let inventory = [
+    {name: "Apple iPhone 15", price: 999.99, quantity: 35, lowStockLevel: 10 },
+    {name: "HP Spectre x360 14", price: 1299.99, quantity: 25, lowStockLevel: 5 },
+    {name: "Apple Watch Series 9", price: 399.99, quantity: 40, lowStockLevel: 10},
+    {name: "Samsung QN90B Neo QLED 4k Smart TV", price: 1299.99, quantity: 20, lowStockLevel: 4},
+    {name: "Beats Fit Pro", price: 199.99. quantity: 30, lowStockLevel: 10}
+
+;]
+
+
