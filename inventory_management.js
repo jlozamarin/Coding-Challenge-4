f64855967f2a0b23931a5f7e21b0683267010db1
Jennifer Loza-Marin <jenniferlozamarin@usf.edu>
2024-09-14 14:25:34 -0400

    Create displayProductDetails function

--- inventory_management.js
+++ inventory_management.js
@@ -2,7 +2,6 @@
 
 // The inventory array contains 5 product objects, each object describes the name, price, quantity in stock, and low stock level number.
 let inventory = [
-let inventory = [
     {name: "Apple iPhone 15", price: 999.99, quantity: 35, lowStockLevel: 10 },
     {name: "HP Spectre x360 14", price: 1299.99, quantity: 25, lowStockLevel: 5 },
     {name: "Apple Watch Series 9", price: 399.99, quantity: 40, lowStockLevel: 10},
